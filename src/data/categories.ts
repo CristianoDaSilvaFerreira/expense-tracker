4f2ca916884f949ff158d385d652fedb7dc394b2
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2021-12-29 21:15:09 -0300

    :rocket: Criandos a estrutura das categorias e tipagem

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const category: Category = {
+    food: { title: 'Alimentação', color: '#fdb032', expense: true },
+    rent: { title: 'Aluguel', color: '#785131', expense: true },
+    salary: { title: 'Salário', color: '#17A031', expense: false },
+}
