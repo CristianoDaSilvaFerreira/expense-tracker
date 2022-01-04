406adbb4518d75fe0cb86b5a6e28732c4e9d7347
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-01-04 20:09:42 -0300

    :up: transferindo  px em rem

--- src/components/TableArea/styles.ts
+++ src/components/TableArea/styles.ts
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 export const Table = styled.table`
     width: 100%;
     background-color: #fff;
-    padding: 20px;
+    padding: 1.25rem;
     box-shadow: 0px 0px 5px #ccc;
-    border-radius: 10px;
-    margin-top: 20px;
+    border-radius: 0.625rem;
+    margin-top: 1.25rem;
 `;
 
     export const TableHeadColumn = styled.th<{ width?: number }>`
     width: ${props => props.width ? `${props.width}px` : 'auto'};
-    padding: 10px 0;
+    padding: 0.625rem 0;
     text-align: left;
 `;
