45bd220e719e4dbfc690ab7c1f6f169707121da5
Cristiano <cristianodevsystem@gmail.com>
2022-03-10 17:23:17 -0300

    :up: Correções

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,13 +1,13 @@
-import { Category } from '../types/Category';
+import { Category } from "../types/Category";
 
 export const categories: Category = {
-    food: { title: 'Alimentação', color: '#fdb032', expense: true },
-    rent: { title: 'Aluguel', color: '#785131', expense: true },
-    salary: { title: 'Salário', color: '#17A031', expense: false },
-    payment: { title: 'Pagamento', color: '#1C7947', expense: false },
-    installment: { title: 'Internet', color: '#FFC900', expense: true },
-    phone_bill: { title: 'Conta Telefônica', color: '#FFA400', expense: false },
-    water_bill: { title: 'Conta de Água', color: '#F0A500', expense: false },
-    energy_bill: { title: 'Conta de Energia', color: '#FEC260', expense: false },
-    other: { title: 'Outros', color: '#FF7272', expense: false },
-}
+  food: { title: "Alimentação", color: "#fdb032", expense: true },
+  rent: { title: "Aluguel", color: "#785131", expense: true },
+  salary: { title: "Salário", color: "#17A031", expense: false },
+  payment: { title: "Pagamento", color: "#1C7947", expense: false },
+  installment: { title: "Internet", color: "#FFC900", expense: true },
+  phone_bill: { title: "Conta Telefônica", color: "#FFA400", expense: false },
+  water_bill: { title: "Conta de Água", color: "#F0A500", expense: false },
+  energy_bill: { title: "Conta de Energia", color: "#FEC260", expense: false },
+  other: { title: "Outros", color: "#FF7272", expense: false },
+};
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,32 +1,32 @@
 /* eslint-disable no-octal */
-import { Item } from '../types/Item';
+import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    // { 
-    //     date: new Date(2021, 12, 4),
-    //     category: 'food',
-    //     title: 'McDonoalds',
-    //     value: 32.65
-    // },
+//   {
+//     date: new Date(2021, 12, 4),
+//     category: "food",
+//     title: "McDonoalds",
+//     value: 32.65,
+//   },
 
-    // { 
-    //     date: new Date(2021, 12, 4),
-    //     category: 'food',
-    //     title: 'Burger King',
-    //     value: 38.00
-    // },
+//   {
+//     date: new Date(2021, 12, 4),
+//     category: "food",
+//     title: "Burger King",
+//     value: 38.0,
+//   },
 
-    // { 
-    //     date: new Date(2021, 12, 4),
-    //     category: 'rent',
-    //     title: 'Aluguel Apt',
-    //     value: 2500.00
-    // },
+//   {
+//     date: new Date(2021, 12, 4),
+//     category: "rent",
+//     title: "Aluguel Apt",
+//     value: 2500.0,
+//   },
 
-    // { 
-    //     date: new Date(2021, 11, 5),
-    //     category: 'salary',
-    //     title: 'Salário ACME',
-    //     value: 6500
-    // },
+//   {
+//     date: new Date(2021, 11, 5),
+//     category: "salary",
+//     title: "Salário ACME",
+//     value: 6500,
+//   },
 ];
